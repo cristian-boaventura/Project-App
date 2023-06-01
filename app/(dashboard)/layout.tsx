50ca24042baec8ac5afe0e8f38a97176169f7992
Cristian Boaventura <hello@cristianboaventura.com>
2023-06-01 21:27:12 +0000

    added Greetings component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardRootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body className="h-screen w-screen rainbow-mesh p-6">
+      <body className="h-screen w-screen candy-mesh p-6">
         <GlassPane className={"w-full h-full flex items-center justify-center"}>
           {children}
         </GlassPane>
